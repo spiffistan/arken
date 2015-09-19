ad6a8fe5cfdda47ea6a860753ddfde8407b40e2f
Anders Aarsæther <aaa@knowit.no>
2015-09-19 22:03:48 +0200

    Annotate

--- app/models/document_description.rb
+++ app/models/document_description.rb
@@ -6,8 +6,8 @@
 #  uuid                         :uuid
 #  title                        :string
 #  description                  :text
-#  document_type                :integer          default("0")
-#  document_status              :integer          default("0")
+#  document_type                :integer          default(0)
+#  document_status              :integer          default(0)
 #  author_id                    :integer
 #  preservation_and_disposal_id :integer
 #  screening_id                 :integer
